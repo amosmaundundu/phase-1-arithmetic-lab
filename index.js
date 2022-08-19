787b0ee1dfc6aadb654973b6f8ceb3bf9d7d8074
amosmaundundu <amosmaundu5@gmail.com>
2022-08-19 19:49:07 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+    let num1 = 31;
+    let num2 = 2;
+    let multiply = num1 * num2;
 
-// Write your code here
+    let random = Math.floor(Math.random()*10)+ 1;
+
+
+    
+  let mod = 25%7;
+  let num3 = 25;
+  let num4 = 7;
+
+ let max = Math.max(5,10,12,17,20);
+   
+    
+    
+    
+      
+    
+        
+    
+        
